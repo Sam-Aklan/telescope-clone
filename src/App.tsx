9dfb34cb2529a90a1fb88be3497eb38e11f34624
sam-abdulilah <sam.mohammed@soilla.com>
2026-01-19 08:30:04 +0300

    clean-some-commits

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,7 @@ function App() {
     <div className="w-full h-screen bg-blue-600"></div>
 
     <CurateSection/>
-    {/* <div className=" w-full h-screen">
-
-    <div className="text-[250px] font-cm-geom-regular w-fit h-fit">
-      Curate
-    </div>
-    </div> */}
-
+   
     <div className="w-full h-screen bg-green-700"></div>
     </>
   );
