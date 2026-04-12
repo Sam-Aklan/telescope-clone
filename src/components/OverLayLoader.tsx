96f624deae3fa133878a07ac77cd3876c76768e1
sam-abdulilah <sam.mohammed@soilla.com>
2026-04-12 22:22:00 +0300

    clean-unused-code

--- src/components/OverLayLoader.tsx
+++ src/components/OverLayLoader.tsx
@@ -1,24 +1,4 @@
-import React from 'react'
- const images =["./pics/leonardo.jpg",
-  "./pics/Mari-curl.jpg",
-  "./pics/albert-einstein.jpg",
-  "./pics/Mozart.jpg",
-  "./pics/frida-kahlo.jpg",
-  "./intro/albert-dera.jpg",
-"./intro/charlie-green.jpg",
-"./intro/christian-buehner.jpg",
-"./intro/abubakr-palestine.jpg",
-"./intro/dad-palstine.jpg",
-"./intro/clint-maliq.jpg",
-"./intro/dajana-reci.jpg",
-"./intro/jean-daniel.jpg",
-"./intro/kevinbidwell.jpg",
-"./intro/old-palestain.jpg",
-"./intro/sara-palestine.jpg",
-"./intro/pexels-hk.jpg",
-"./zoom/stefan-moertl.jpg",
-"./zoom/stefan-moertl.png",
-]
+
 const OverLayLoader = ({progress}:{progress:number}) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black text-white">
